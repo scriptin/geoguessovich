ef520b1b85ddc36baabcf2ae0ef8a557a882a5d3
Dmitry Shpika <scrptn@gmail.com>
2021-05-30 18:52:38 +0300

    Implement basic autocomplete

--- app/app.js
+++ app/app.js
@@ -1,3 +1,62 @@
+async function loadCountriesData(progressBar) {
+    const response = await fetch('countries.json');
+    const countries = await response.json();
+    progressBar.style.width = '1%'; // because countries data has been loaded
+
+    return countries;
+}
+
+async function loadCitiesData(countryCodes, progressBar) {
+    const cities = {};
+
+    // Loading in parallel
+    const requests = [];
+    for (const code of countryCodes) {
+        requests.push(
+            fetch(`cities/${code}.json`)
+                .then(data => data.json())
+                .then(json => {
+                    cities[code] = json;
+                    const total = countryCodes.length + 1; // +1 for countries data
+                    const loaded = Object.keys(cities).length + 1;
+                    const percent = Math.trunc((loaded / total) * 100);
+                    progressBar.style.width = `${percent}%`;
+                })
+                .catch(e => console.error(e))
+        );
+    }
+    await Promise.all(requests);
+
+    return cities;
+}
+
+function createAutocompleteItem(countryData, ordinalNumber) {
+    const element = document.createElement('div');
+    element.classList.add('autocomplete-item');
+
+    if (!countryData) {
+        element.classList.add('nothing-found');
+        element.appendChild(document.createTextNode('Nothing found'));
+    } else {
+        const [domain, name, ...otherNames] = countryData;
+
+        const kbdEl = document.createElement('kbd');
+        kbdEl.appendChild(document.createTextNode(ordinalNumber))
+        element.appendChild(kbdEl);
+
+        const domainEl = document.createElement('code');
+        domainEl.appendChild(document.createTextNode(domain));
+        element.appendChild(domainEl);
+
+        const allNames = [name, ...otherNames].join(', ');
+        const namesEl = document.createElement('span');
+        namesEl.appendChild(document.createTextNode(allNames));
+        element.appendChild(namesEl);
+    }
+
+    return element;
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
     const loading = document.getElementById('loading');
     const progressBar = loading.getElementsByClassName('progress-bar')[0];
@@ -7,33 +66,39 @@ window.addEventListener('DOMContentLoaded', async () => {
     loading.classList.remove('hide');
     app.classList.add('hide');
 
-    const response = await fetch('countries.json');
-    const countries = await response.json();
-    progressBar.style.width = '1%';
-    const countryCodes = Object.keys(countries).filter(code => !['_comment', 'vi', 'cx'].includes(code));
-    const cities = {};
+    const countries = await loadCountriesData(progressBar);
+    const countryCodes = Object.keys(countries)
+        .filter(code => !['_comment', 'vi', 'cx'].includes(code))
+        .sort();
 
-    const updateProgressBar = () => {
-        const total = countryCodes.length + 1;
-        const loaded = Object.keys(cities).length + 1;
-        const percent = Math.trunc((loaded / total) * 100);
-        progressBar.style.width = `${percent}%`;
-    }
-
-    const requests = [];
-    for (const code of countryCodes) {
-        requests.push(
-            fetch(`cities/${code}.json`)
-                .then(data => data.json())
-                .then(json => {
-                    cities[code] = json;
-                    updateProgressBar();
-                })
-                .catch(e => console.error(e))
-        );
-    }
-    await Promise.all(requests);
+    const cities = await loadCitiesData(countryCodes, progressBar);
 
     loading.classList.add('hide');
     app.classList.remove('hide');
+
+    const countryInput = document.getElementById('county-input');
+    const autocomplete = document.getElementById('autocomplete');
+
+    countryInput.addEventListener('input', (e) => {
+        const value = e.target.value
+            .toLowerCase()
+            .trim()
+            .replaceAll(/\s{2,}/gi, ' ');
+        const valueIsEmpty = value === '' || value === '.';
+        const matchingCodes = valueIsEmpty ? [] : countryCodes.filter(code => {
+            const [domain, ...names] = countries[code];
+            return code.startsWith(value)
+                || domain.startsWith(value)
+                || domain === `.${value}`
+                || !!names.find(n => n.toLowerCase().includes(value));
+        }).slice(0, 9);
+        autocomplete.innerHTML = '';
+        if (matchingCodes.length > 0) {
+            matchingCodes.forEach((code, index) => {
+                autocomplete.appendChild(createAutocompleteItem(countries[code], index + 1));
+            });
+        } else if (!valueIsEmpty) {
+            autocomplete.appendChild(createAutocompleteItem(null));
+        }
+    });
 });
